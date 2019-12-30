bb24c0b6bbf6d5aec7a85355972aa65555b010f7
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-12-30 08:17:06 -0600

    Command-line will migrate Flyway
    This is a temporary hack for the DB run script.

--- kotlin-micronaut/run-db.sh
+++ kotlin-micronaut/run-db.sh
@@ -1,2 +1,5 @@
 # Assumes no processing needed for SQL file
-exec docker run -p 5432:5432 -e TZ=Etc/UTC -v "$(git rev-parse --show-toplevel)/kotlin-micronaut/src/main/resources/db/migration":/docker-entrypoint-initdb.d postgres:12
+case $# in
+0 ) exec docker run -p 5432:5432 -e TZ=Etc/UTC -v "$(git rev-parse --show-toplevel)/kotlin-micronaut/src/main/resources/db/migration":/docker-entrypoint-initdb.d postgres:12 ;;
+* ) exec docker run -p 5432:5432 -e TZ=Etc/UTC postgres:12 ;;
+esac
