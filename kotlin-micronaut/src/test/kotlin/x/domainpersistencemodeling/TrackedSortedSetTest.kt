812c47cf55f64a5aecf2ff1ef113e94a8fb777f9
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-01-08 06:45:25 -0600

    Raise coverage (summary number still unchanged)

--- kotlin-micronaut/src/test/kotlin/x/domainpersistencemodeling/TrackedSortedSetTest.kt
+++ kotlin-micronaut/src/test/kotlin/x/domainpersistencemodeling/TrackedSortedSetTest.kt
@@ -10,6 +10,36 @@ private val doNothing: (String, MutableSet<String>) -> Unit = { _, _ -> }
 
 internal class TrackedSortedSetTest {
     @Test
+    fun `should do nothing when empty`() {
+        val tracked = TrackedManyToOne(setOf(), doNothing, doNothing)
+
+        expect(tracked.mutated { true }).toBe(false)
+    }
+
+    @Test
+    fun `should do a little when changed`() {
+        val tracked = TrackedManyToOne(setOf("ABC"), doNothing, doNothing)
+
+        expect(tracked.mutated { true }).toBe(true)
+    }
+
+    @Test
+    fun `should notice first element changed`() {
+        val tracked =
+            TrackedManyToOne(setOf("ABC", "DEF"), doNothing, doNothing)
+
+        expect(tracked.mutated { it == "ABC" }).toBe(true)
+    }
+
+    @Test
+    fun `should notice second element changed`() {
+        val tracked =
+            TrackedManyToOne(setOf("ABC", "DEF"), doNothing, doNothing)
+
+        expect(tracked.mutated { it == "DEF" }).toBe(true)
+    }
+
+    @Test
     fun `should notify when deleting through iterator`() {
         var removed = false
         val tracked = TrackedManyToOne(setOf("ABC"), doNothing) { _, _ ->
