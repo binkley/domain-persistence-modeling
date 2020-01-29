84d1f02b4714de286d5104cc9a118182fe788a1e
Brian Oxley <boxley@thoughtworks.com>
2020-01-29 09:46:45 -0600

    Update Postgres

--- kotlin-micronaut/run-db.sh
+++ kotlin-micronaut/run-db.sh
@@ -1,5 +1,5 @@
 # Assumes no processing needed for SQL file
 case $# in
-0 ) exec docker run -p 5432:5432 -e TZ=Etc/UTC -v "$(git rev-parse --show-toplevel)/kotlin-micronaut/src/main/resources/db/migration":/docker-entrypoint-initdb.d postgres:12 ;;
-* ) exec docker run -p 5432:5432 -e TZ=Etc/UTC postgres:12 ;;
+0) exec docker run -p 5432:5432 -e TZ=Etc/UTC -v "$(git rev-parse --show-toplevel)/kotlin-micronaut/src/main/resources/db/migration":/docker-entrypoint-initdb.d postgres:12.1 ;;
+*) exec docker run -p 5432:5432 -e TZ=Etc/UTC postgres:12.1 ;;
 esac
