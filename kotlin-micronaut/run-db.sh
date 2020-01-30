c0112cdbfebca78bfde79248ba8d0e29737f08ef
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-01-29 18:28:24 -0600

    Fix Docker versions

--- kotlin-micronaut/run-db.sh
+++ kotlin-micronaut/run-db.sh
@@ -1,5 +1,5 @@
 # Assumes no processing needed for SQL file
-image=postgres-tableversion:12-1.7.1
+image=postgres-tableversion:12.1-1.7.1
 
 case $# in
 0) exec docker run -p 5432:5432 -e TZ=Etc/UTC -v "$(git rev-parse --show-toplevel)/kotlin-micronaut/src/main/resources/db/migration":/docker-entrypoint-initdb.d $image ;;
