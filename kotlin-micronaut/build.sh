daf919269d30a6f745b7ad4a656d166206daf4cf
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-07-18 08:23:06 -0500

    Mid refactoring
    Committing so it can be reverted, and then unreverted if good things happen.

--- kotlin-micronaut/build.sh
+++ kotlin-micronaut/build.sh
@@ -3,5 +3,5 @@
 set -e
 
 # TODO: Lie to testcontainers that local custom image is the real one
-docker build . -t postgresql:12.1-tableversion-1.7.1
+docker build . -t postgresql:12.1-tableversion-1.8.0
 ./mvnw clean verify "$@"
